b98db802a2c316cfeaf913851a4a1835d3ac59b1
tlephan <phanlethanh93@gmail.com>
2026-03-04 18:16:54 +0700

    feat upgrade latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ['class'],
-  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       colors: {
